1eb4ba83007cbd69fbcac44c017a91f3058f5db2
drradicevic <drradicevic@gmail.com>
2024-05-25 08:16:26 +0200

    Add fetch and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import TodoList from './ToDoList.jsx';
 import {todos} from "./todos.js"
 import Header from './Header.jsx';
-import {useState} from "react";
+import {useState, useEffect } from "react";
 
 
 
 function App() {
+
+  fetch('https://dummyjson.com/todos')
+.then(res => res.json())
+.then(console.log);
+
+
+  const url = "https://jsonplaceholder.typicode.com/users";
+const [data, setData] = useState([]);
+
+const fetchInfo = () => {
+  return fetch(url)
+    .then((res) => res.json())
+    .then((d) => setData(d))
+}
+
+
+useEffect(() => {
+  fetchInfo();
+}, []);
+
+
     const [doneTasks, setDoneTasks] = useState([]);
 
     function onTodoItemClick (id) {
@@ -24,7 +45,6 @@ function App() {
       setDoneTasks(newDoneState)
 }
 
-    console.log(doneTasks)
 
 
   return (
@@ -32,6 +52,9 @@ function App() {
    <Header />
     {/*todos.map((item) => <TodoList className={} id={item.id} label={item.label} createdAt={item.createdAt} onClick={() => onTodoItemClick(item.id)} />)*/}
     {todos.map((item) => <TodoList isIncludes={doneTasks.includes(item.id)}  key={item.id} id={item.id} label={item.label} createdAt={item.createdAt} onSelect={() => onTodoItemClick(item.id)} />)}      
+    <Header />
+    {data.map((item) => <TodoList isIncludes={doneTasks.includes(item.id)}  key={item.id} id={item.id} label={item.name} createdAt={item.address.city} onSelect={() => onTodoItemClick(item.id)} />)}      
+
    </>
   );
 }
--- src/todos.js
+++ src/todos.js
@@ -1,27 +1,32 @@
-export const todos = [{
+export const todos = [
+  {
     id: 1,
-    label: 'Todo 1',
-    createdAt: '1.2.2024',
-},
-    {
-        id: 2,
-        label: 'Todo 2',
-        createdAt: '1.2.2024',
-    },{
-        id: 3,
-        label: 'Todo 3',
-        createdAt: '1.2.2024',
-    },{
-        id: 4,
-        label: 'Todo 4',
-        createdAt: '1.2.2024',
-    },{
-        id: 5,
-        label: 'Todo 5',
-        createdAt: '1.2.2024',
-    },{
-        id: 6,
-        label: 'Todo 6',
-        createdAt: '1.2.2024',
-    }
-    ];
+    label: "Todo 1",
+    createdAt: "1.2.2024",
+  },
+  {
+    id: 2,
+    label: "Todo 2",
+    createdAt: "1.2.2024",
+  },
+  {
+    id: 3,
+    label: "Todo 3",
+    createdAt: "1.2.2024",
+  },
+  {
+    id: 4,
+    label: "Todo 4",
+    createdAt: "1.2.2024",
+  },
+  {
+    id: 5,
+    label: "Todo 5",
+    createdAt: "1.2.2024",
+  },
+  {
+    id: 6,
+    label: "Todo 6",
+    createdAt: "1.2.2024",
+  },
+];
